501f1f271b62b968f98c95249b3405d61f906146
Lior Roytburd <43315313+liorroy@users.noreply.github.com>
2023-09-25 18:08:20 +0300

    Update BigQuerySQLScript.sql

--- BigQuerySQLScript.sql
+++ BigQuerySQLScript.sql
@@ -1,6 +1,8 @@
-Zen City's Journey through London's bike rental data – BigQuery SQL script:
+-- Zen City's Journey through London's bike rental data
+
 
 --Data Cleaning & Data Wrangling:
+
 
 SELECT
 *
@@ -8,15 +10,18 @@ FROM
 `data-analysis-389112.Project_Google.cycle_hire_new`
 LIMIT 1000;
 
+
 SELECT
 COUNT(*)
 FROM
 `data-analysis-389112.Project_Google.cycle_hire_new`; --49015
 
+
 SELECT
 COUNT(DISTINCT rental_id)
 FROM
 `data-analysis-389112.Project_Google.cycle_hire_new`; --49015
+
 
 SELECT
 *
@@ -24,16 +29,20 @@ FROM
 `data-analysis-389112.Project_Google.cycle_stations_pro`
 LIMIT 1000;
 
+
 SELECT
 COUNT(*)
 FROM
 `data-analysis-389112.Project_Google.cycle_stations_pro`; --795
 
 
+
+
 SELECT
 COUNT(DISTINCT id)
 FROM
 `data-analysis-389112.Project_Google.cycle_stations_pro`; --795
+
 
 --Check if we have duplicate station id's:
 SELECT
@@ -43,9 +52,11 @@ FROM
 GROUP BY id
 HAVING COUNT(id) > 1; --no
 
+
 --num of bikes:
 SELECT COUNT(DISTINCT bike_id)
 FROM `data-analysis-389112.Project_Google.cycle_hire_new`; --11185
+
 
 --Checking if the values in column duration are correct:
 SELECT rental_id
@@ -58,6 +69,7 @@ TIMESTAMP_DIFF(end_date, start_date, SECOND) AS calculated_difference
 FROM
 `data-analysis-389112.Project_Google.cycle_hire_new`)
 WHERE duration != calculated_difference; --there are no issues in terms of duration
+
 
 --Checking if we have invalid rides in terms of station, rides that are in stations which have already been removed:
 SELECT *
@@ -76,6 +88,7 @@ FROM
 `data-analysis-389112.Project_Google.cycle_stations_pro`
 WHERE installed = false OR removal_date IS NOT NULL); --127 invalid rides that must be removed
 
+
 --Check if we have 2 stations with the same location:
 SELECT latitude, longitude, COUNT(*)
 FROM
@@ -88,7 +101,10 @@ GROUP BY latitude, longitude
 HAVING COUNT(*) > 1; --No!
 
 
+
+
 --Handle station names with double spaces:
+
 
 SELECT
 name,
@@ -96,6 +112,8 @@ replace (name,'  ',' ')
 FROM
 `data-analysis-389112.Project_Google.cycle_stations_pro`
 WHERE INSTR(name,'  ') > 0 ; --3 stations that should be fixed
+
+
 
 
 --Outliers in terms of ride duration:
@@ -117,6 +135,7 @@ FROM `data-analysis-389112.Project_Google.cycle_hire_new`)
 - 3 * (SELECT STDDEV(duration)
 FROM `data-analysis-389112.Project_Google.cycle_hire_new` ); --112 outliers
 
+
 --Check for stations that exists in the rides table but not in the stations table:
 SELECT
 DISTINCT r.end_station_id
@@ -124,6 +143,7 @@ FROM `data-analysis-389112.Project_Google.cycle_hire_new` AS r
 LEFT JOIN `data-analysis-389112.Project_Google.cycle_stations_pro` AS e
 ON r.end_station_id = e.id
 WHERE e.id IS NULL; --15 invalid stations
+
 
 --There are 774 rides that are invalid in terms of invalid ending station:
 SELECT rental_id
@@ -136,13 +156,16 @@ LEFT JOIN `data-analysis-389112.Project_Google.cycle_stations_pro` AS e
 ON r.end_station_id = e.id
 WHERE e.id IS NULL);
 
+
 --Check for nulls in new table
 SELECT DISTINCT bike_model,end_station_logical_terminal,start_station_logical_terminal,end_station_priority_id
 FROM `data-analysis-389112.Project_Google.cycle_hire_new`; -- all this columns are irrelevant
 
+
 --Check values in column Locked:
 SELECT DISTINCT locked
 FROM`data-analysis-389112.Project_Google.cycle_stations_pro`; --all stations are unlocked!
+
 
 --Check for duration miss calculation
 SELECT rental_id
@@ -156,10 +179,12 @@ FROM
 `data-analysis-389112.Project_Google.cycle_hire_new`)
 WHERE duration != calculated_difference; --Duration values are valid!
 
+
 -- Ensure data integrity for the "start_station_id" and"end_station_id" columns?
 SELECT COUNT(*) AS missing_station_id_count
 FROM `data-analysis-389112.Project_Google.cycle_hire_new`
 WHERE start_station_id IS NULL OR end_station_id IS NULL; --there are no rows with null values for those columns
+
 
 --The cte + Staistics:
 --Used Inner Join to remove the 15 ending stations that appear in ride table but are missing from the station table (*removed 774)
@@ -222,7 +247,7 @@ APPROX_QUANTILES(duration_in_minutes, 2)[OFFSET(1)] AS median_duration_minutes,
 ROUND(MIN(duration_in_minutes),2) as min_duration_minutes,
 ROUND(MAX(duration_in_minutes),2) as max_duration_minutes
 FROM table_cleaned;
- 
+
 --Basic Statistics to start the presentaion:
 WITH table_cleaned AS
 (SELECT
@@ -273,6 +298,7 @@ FROM `data-analysis-389112.Project_Google.cycle_hire_new`)
 - 3 * (SELECT STDDEV(duration)
 FROM `data-analysis-389112.Project_Google.cycle_hire_new` )))
 
+
 SELECT
 starting_name,
 COUNT(*) / 1000 AS total_rides_per_station_in_thousands,
@@ -282,10 +308,12 @@ APPROX_QUANTILES(duration_in_minutes, 2)[OFFSET(1)] AS median_duration_minutes
 FROM table_cleaned
 GROUP BY starting_name
 ORDER BY AVG_AIRlength_distance;
- 
+
+
 
 --First Question: Does the distance from the center of London have an affect on the utilization of the station?
 --London city center location: (latitude and longitude values) https://www.findlatitudeandlongitude.com/l/London+city+centre/5715707/
+
 
 --First of all, here are all the stations that exist but have had no rides during Q1 of 2021:
 SELECT id
@@ -298,7 +326,8 @@ FROM
 JOIN
 `data-analysis-389112.Project_Google.cycle_hire_new` AS r
 ON s.id = r.start_station_id OR s.id = r.end_station_id);--there are 27 stations that have no usage.
- 
+
+
 
 --The average distance from London city center of the 27 stations that have no rides:
 SELECT AVG(distance_from_london_center_in_km) AS average_distance_from_london_center_in_km
@@ -314,6 +343,7 @@ FROM
 JOIN
 `data-analysis-389112.Project_Google.cycle_hire_new` AS r
 ON s.id = r.start_station_id OR s.id = r.end_station_id));--5.68 km
+
 
 --Now, let’s find the average distance from London city center of the top 27 stations (by the number of rides during Q1 2021):
 --The subquery: *select the top 27 stations by the num of rides:
@@ -338,8 +368,10 @@ GROUP BY end_station_id
 ORDER BY COUNT(*) DESC
 LIMIT 28)); --2.35 km
 
+
 --Next, we visualized our results using BigQuery Geo Viz: a web tool for visualization of geospatial data in BigQuery using Google Maps APIs.
 https://cloud.google.com/bigquery/docs/geospatial-get-started
+
 
 --For GeoViz:
 --For each of the 54 (27 worst + 27 best) stations, we will return it id, name, geopoint and distance from the center of London
@@ -356,7 +388,9 @@ GROUP BY end_station_id
 ORDER BY COUNT(*) DESC
 LIMIT 28)
 
+
 UNION ALL
+
 
 SELECT id, name, ST_GEOGPOINT(longitude, latitude) AS geo_point, ROUND(ST_DISTANCE(ST_GEOGPOINT(longitude, latitude), ST_GEOGPOINT(-0.1277, 51.507391))) / 1000 AS distance_from_london_center_in_km, "The 27 empty stations" AS type
 FROM
@@ -369,10 +403,12 @@ JOIN
 `data-analysis-389112.Project_Google.cycle_hire_new` AS r
 ON s.id = r.start_station_id OR s.id = r.end_station_id)
 
+
 UNION ALL
 
+
 SELECT 0, "London city center", ST_GEOGPOINT(-0.1277, 51.507391) AS geo_point, ROUND(ST_DISTANCE(ST_GEOGPOINT(-0.1277, 51.507391), ST_GEOGPOINT(-0.1277, 51.507391))) / 1000 AS distance_from_london_center_in_km, "London city center";
- 
+
   --Second Question: Find the best and worst Starting stations in terms of average amount of daily rides, the average duration of those rides, and the dock count of each station(Multivariate)
 --Using our findings we are able to find the differences in utilization between each of the 6 starting stations:
 WITH table_cleaned AS
@@ -459,7 +495,9 @@ GROUP BY start_station_id,starting_name --Group by each starting station
 ) AS the_table
 JOIN geo_for_each_starting_station --So we can get the GeoPoint for each starting station (each row)
 ON the_table.start_station_id = geo_for_each_starting_station.start_station_id;
- 
+
+
+
 
 
 --Third Question: Analyze rental patterns by day of the week and hour
@@ -513,6 +551,7 @@ FROM `data-analysis-389112.Project_Google.cycle_hire_new`)
 - 3 * (SELECT STDDEV(duration)
 FROM `data-analysis-389112.Project_Google.cycle_hire_new` )))
 
+
 SELECT
 start_dayofweek, --+1 beacuse the date value is in UTC, and London is one hour ahead:
 COUNT(CASE WHEN start_hour+1 IN (6,7,8,9,10,11,12) THEN 1 END) AS Morning,
@@ -523,7 +562,7 @@ FROM --Morning - 6 to 12 am, Afternoon - 1 to 6 pm, Evening - 7 to 10 pm, night 
 table_cleaned
 GROUP BY start_dayofweek
 ORDER BY start_dayofweek;
- 
+
 --The second query will return the average ride duration in minutes for each day of the week and time of day:
 WITH table_cleaned AS
 (SELECT
@@ -574,6 +613,7 @@ FROM `data-analysis-389112.Project_Google.cycle_hire_new`)
 - 3 * (SELECT STDDEV(duration)
 FROM `data-analysis-389112.Project_Google.cycle_hire_new` )))
 
+
 SELECT
 start_dayofweek, --+1 beacuse the date value is in UTC, and London is one hour ahead:
 ROUND(AVG(CASE WHEN start_hour+1 IN (6,7,8,9,10,11,12) THEN duration_in_minutes END),2) AS Morning,
@@ -585,7 +625,8 @@ FROM
 table_cleaned
 GROUP BY start_dayofweek
 ORDER BY start_dayofweek;
- 
+
+
 
 --Prediction: Predict how many rentals will be made in the next month (April 2021) in “Albert Gate, Hyde Park” bike station.
 --Albert Gate, Hyde Park - ID: 303
@@ -649,7 +690,7 @@ FROM
 WHERE name LIKE '%Albert Gate, Hyde Park%')
 GROUP BY day
 ORDER BY day;
- 
+
 --Query for the number of rides in the staion for each month:
 WITH table_cleaned AS
 (SELECT
@@ -710,16 +751,16 @@ FROM
 WHERE name LIKE '%Albert Gate, Hyde Park%')
 GROUP BY start_month
 ORDER BY start_month;
- 
- 
---We will move this table to sheets, and download is as an excel file so we could load it into gretl - a statistical package able to run a linear regression (ordinary least squares (OLS) model):  
+
+
+--We will move this table to sheets, and download is as an excel file so we could load it into gretl - a statistical package able to run a linear regression (ordinary least squares (OLS) model): 
 The equation: num_of_rides = α + β1 * time + β2 * time ^ 2
- 
+
 now, we use of model to predict - forecast the next 30 days: the month of April 2020:
- 
+
 (with a significance level of α = 0.01 -> 1-α = 99%)
- 
+
 now, all that’s left is to sum the predicted values of April 2021 and we’ll receive an answer: 8,836 rides during April 2021!
- 
+
 
 
